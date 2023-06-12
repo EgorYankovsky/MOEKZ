8be0c808037e9b12d2158a13e4db4fd1b3e70aa7
Егор Янковский <yankovskij.2019@azure.nstu.ru>
2023-06-12 23:37:30 +0700

    Number 3

--- Task3.py
+++ Task3.py
@@ -1,14 +1,14 @@
 import numpy as np
 from scipy.optimize import minimize
-import pandas as pd
+import numdifftools as nd
 
-def pirson(f, grad, eta_alg, x0):
+def pirson(f, eta_alg, x0):
     x_curr = x0
     x_next = x0
     eta = np.eye(2)
     for i in range(3):
         x_curr = x_next
-        df = grad(x_curr)
+        df = nd.Gradient(f)(x_curr)
         to_minimize = lambda l: f(x_curr - l * np.dot(eta, df))
         lk = minimize(to_minimize, x0=0, tol=0.03).x[0]
         Sk = -np.dot(eta, df)
@@ -20,4 +20,4 @@ def pirson(f, grad, eta_alg, x0):
         print("Sk:", Sk)
         print("")
         x_next = x_curr + lk * Sk
-        eta = eta_alg(x_next - x_curr, grad(x_next) - grad(x_curr), eta)
+        eta = eta_alg(x_next - x_curr, nd.Gradient(f)(x_next) - nd.Gradient(f)(x_curr), eta)
