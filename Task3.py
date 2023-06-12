bf975bbbafac5658f11722e44ac20bee6de54c24
Егор Янковский <yankovskij.2019@azure.nstu.ru>
2023-06-12 16:50:43 +0700

    Problems fixed

--- Task3.py
+++ Task3.py
@@ -2,22 +2,12 @@ import numpy as np
 from scipy.optimize import minimize
 import pandas as pd
 
-# Вводим градиент функции
-def grad(x):
-    return np.array([22 * x[0] + 60 - 36 * x[1],        # dF/dx
-                     88 * x[1] - 200 - 36 * x[0]])      # dF/dy
-
-
-# Вводим функцию
-def f(x):
-    return (x[0] + 2 * x[1] - 10)**2 + 10 * (x[0] - 2 * x[1] + 4)**2
-
 def new_eta(dxk, dgk, eta):
     temp = np.dot(eta, dgk)
     t = np.dot(dxk - temp, np.transpose(temp)) / np.dot(np.transpose(dgk), temp)
     return eta + t
 
-def pirson(x0):
+def pirson(f, grad, x0):
     x_curr = x0
     x_next = x0
     eta = np.eye(2)
@@ -36,6 +26,3 @@ def pirson(x0):
         print("")
         x_next = x_curr + lk * Sk
         eta = new_eta(x_next - x_curr, grad(x_next) - grad(x_curr), eta)
-
-x0 = np.array([1, 0])
-pirson(x0)
